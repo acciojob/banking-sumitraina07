8817071859f81ee9e19f6bee25d3a6d08fd9f0df
sumeetraina <sumitraina07@gmail.com>
2023-09-23 04:50:59 +0530

    Initial Commit

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -6,26 +6,60 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
+    public BankAccount() {
+    }
 
+    public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        if(9*digits < sum){
+            throw new Exception("Account Number can not be generated");
+        }
 
-        return null;
+        String accountNumber = "";
+        while(sum > 9){
+            accountNumber = accountNumber + '9';
+            sum -= 9;
+        }
+        accountNumber = accountNumber + (sum + "");
+        while(accountNumber.length() < digits){
+            accountNumber += '0';
+        }
+        return accountNumber;
     }
 
     public void deposit(double amount) {
         //add amount to balance
-
+        balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        if((balance - amount) >= minBalance){
+            balance -= amount;
+        }
+        else{
+            throw new Exception("Insufficient Balance");
+        }
     }
 
 }
