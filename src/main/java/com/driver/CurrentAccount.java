0723dd9ab4a03b48f34bdb72570a555f3c97ded5
sumeetraina <sumitraina07@gmail.com>
2023-10-04 04:04:29 +0530

    myCommit

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -7,6 +7,10 @@ public class CurrentAccount extends BankAccount{
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
         super(name, balance,5000);
         this.tradeLicenseId = tradeLicenseId;
+
+        if(balance < 5000){
+            throw new Exception("Insufficient Balance");
+        }
     }
 
     public void validateLicenseId() throws Exception {
@@ -15,87 +19,106 @@ public class CurrentAccount extends BankAccount{
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
-        if(!isNumberValid(tradeLicenseId)){
+        //first checking the validity
 
-            String rearrangedId = arrangeString(tradeLicenseId);
-            if(rearrangedId == ""){
+        if(!isLicenseNumberValid(tradeLicenseId)){
+
+            //need to generate valid string
+
+            //throw a validation if cannot create licenseId
+            if(!canCreateLicense(tradeLicenseId)){
+
                 throw new Exception("Valid License can not be generated");
             }
-            else {
-                this.tradeLicenseId = rearrangedId;
-            }
+
+            //if we can create then we call rearrange
+             rearrangeLicenseId(tradeLicenseId);
+
+        }
+        else{
+            this.tradeLicenseId = tradeLicenseId;
         }
     }
 
-    public char getCountChar(int[] count){
+    public boolean canCreateLicense(String licenseId){
 
-        int max = 0;
-        char ch = 0;
-        for(int i=0; i<26;i++){
-            if(count[i] > max){
-                max = count[i];
-                ch = (char)((int)'A' + i);
+        int[] freq = new int[26];
+
+        char maxFreqChar = '0';
+        int maxFreq = 0;
+
+        for(int i=0; i<licenseId.length(); i++){
+            char ch = licenseId.charAt(i);
+            freq[ch - 'A']++;
+
+            if(freq[ch-'A'] > maxFreq){
+                maxFreqChar = ch;
+                maxFreq = freq[ch - 'A'];
             }
         }
-        return ch;
+        if(freq[maxFreqChar] > (licenseId.length() + 1)/2){
+            return false;
+        }
+        return true;
     }
 
-    private String arrangeString(String S) {
+    public String rearrangeLicenseId(String s){
 
-        int N = S.length();
+        //finding freq of chars
+        int[] freq = new int[26];
 
-        int[] count = new int[26];
-        for(int i=0; i<26; i++){
-            count[i] = 0;
-        }
-        for(char ch : S.toCharArray()){
-            count[(int) ch - (int) 'A']++;
-        }
+        char maxFreqChar = '0';
+        int maxFreq = 0;
 
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            freq[ch - 'A']++;
 
-        char ch_max = getCountChar(count);
-        int maxCount = count[(int) ch_max- (int) 'A'];
-
-        if(maxCount > (N+1)/2){
-            return "";
-        }
-        String res = "";
-        for(int i=0; i<N; i++){
-            res += ' ';
-        }
-
-        int ind = 0;
-        while(maxCount > 0){
-            res = res.substring(0, ind) + ch_max
-                    + res.substring(ind + 1);
-
-            ind = ind + 2;
-            maxCount--;
-        }
-        count[(int)ch_max - (int)'A'] = 0;
-        for(int i=0; i<26; i++){
-            while(count[i] > 0){
-                ind = (ind>=N) ? 1 : ind;
-                res = res.substring(0, ind)
-                        + (char) ((int)'A' + 1)
-                        + res.substring(ind + 1);
-                ind = ind + 2;
-                count[i]--;
+            if(freq[ch-'A'] > maxFreq){
+                maxFreqChar = ch;
+                maxFreq = freq[ch - 'A'];
             }
         }
-        return res;
+        // put in the result string
+        char[] result = new char[s.length()];
+
+        int index = 0;
+        //put on even places frst (maxFreqChar)
+        while(freq[maxFreqChar - 'A'] > 0){
+
+            result[index] = maxFreqChar;
+            index += 2; //only even places
+            freq[maxFreqChar]--;
+        }
+        //Iterate over all the remaining characters
+
+        for(int i=0; i<27;i++){
+            int frequency = freq[i]; //get freq of each element
+
+            while(frequency > 0){
+                //keep adding to the result
+                if(index >= s.length()){
+                    index = 1;
+                }
+
+                result[index] = (char)(i+'A');
+                index +=2; //for all odd places
+
+            }
+        }
+        String str = String.valueOf(result);
+        return str;
     }
 
-    private boolean isNumberValid(String licenseId) {
-        for(int i=0; i < licenseId.length() - 1; i++){
+    public boolean isLicenseNumberValid(String licenseId){
+
+        for(int i=0;i<licenseId.length();i++){
             if(licenseId.charAt(i) == licenseId.charAt(i+1)){
                 return false;
             }
         }
         return true;
     }
-
-
 
     public String getTradeLicenseId() {
         return tradeLicenseId;
--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -10,11 +10,8 @@ public class StudentAccount extends BankAccount{
         this.institutionName = institutionName;
     }
 
+
     public String getInstitutionName() {
         return institutionName;
-    }
-
-    public void setInstitutionName(String institutionName) {
-        this.institutionName = institutionName;
     }
 }
